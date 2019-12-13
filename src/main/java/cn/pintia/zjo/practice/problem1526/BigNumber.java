943739633db769d3ee2696a226c705181014f573
empiregeneral <13777470884@qq.com>
2019-12-13 18:13:49 +0800

    吐槽吐槽

---
+++ src/main/java/cn/pintia/zjo/practice/problem1526/BigNumber.java
@@ -0,0 +1,21 @@
+package cn.pintia.zjo.practice.problem1526;
+
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+
+/**
+ * @ClassName: BigNumber
+ * @Description: zju 1526
+ * 实际上是double类型的精度计算问题
+ * @Author :lei.zhu
+ * @Date 2019/12/13 17:19
+ * @Version 1.0
+ **/
+public class BigNumber implements Readable {
+
+    @Override
+    public int read(CharBuffer cb) throws IOException {
+        return 0;
+    }
+}
---
+++ src/main/java/cn/pintia/zjo/practice/problem1526/Main.java
@@ -0,0 +1,11 @@
+package cn.pintia.zjo.practice.problem1526;
+
+/**
+ * @ClassName: Main
+ * @Description: TODO
+ * @Author :lei.zhu
+ * @Date 2019/12/13 17:19
+ * @Version 1.0
+ **/
+public class Main {
+}
