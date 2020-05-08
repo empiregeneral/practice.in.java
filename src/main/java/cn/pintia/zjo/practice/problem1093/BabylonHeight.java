81f251c317ae87f6a22b2134470d1441df7c0cc2
empiregeneral <13777470884@qq.com>
2020-05-09 00:57:44 +0800

    [MOD] zjo 1093 dp bug修复

--- src/main/java/cn/pintia/zjo/practice/problem1093/BabylonHeight.java
+++ src/main/java/cn/pintia/zjo/practice/problem1093/BabylonHeight.java
@@ -19,12 +19,12 @@ public class BabylonHeight {
         int[] heightArr = new int[boxes.length];
 
         heightArr[0] = boxes[0].getHeight();
-        for (int i = 1; i < boxes.length; i++) {
+        for (int i = 0; i < boxes.length; i++) {
             int height = 0;
-            for (int j = 0; j < i; j++) {
-                if ((boxes[j].getLength() > boxes[i].getLength()) &&
-                        (boxes[j].getWidth() > boxes[j].getWidth()) &&
-                        (heightArr[j] > height)) {
+            for (int j = i - 1; j >= 0; j--) {
+                if ((boxes[i].getLength() < boxes[j].getLength()) &&
+                        (boxes[i].getWidth() < boxes[j].getWidth()) &&
+                        height < heightArr[j]) {
                     height = heightArr[j];
                 }
             }
