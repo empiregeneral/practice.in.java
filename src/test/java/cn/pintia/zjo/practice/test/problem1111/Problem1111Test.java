0976922d8f4db7f3ddd18762665e12578be59ddc
empiregeneral <13777470884@qq.com>
2020-05-13 14:38:52 +0800

    [ADD] 修复allure report history trend显示的问题

--- src/test/java/cn/pintia/zjo/practice/test/problem1111/Problem1111Test.java
+++ src/test/java/cn/pintia/zjo/practice/test/problem1111/Problem1111Test.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Problem1111Test {
 
     @Test
-    @Parameters({"5C 5D 2C 2D AS,2S 2H 3C 3D AD,Black wins.","AH KH QH JH 3C,2S 2H 2C 3H TS,Black wins."})
+    @Parameters({"5C 5D 2C 2D AS,2S 2H 3C 3D AD,Black wins.","AH KH QH JH 3C,2S 2H 2C 3H TS,White wins."})
     public void testJudgement(String blackHands, String whiteHands, String result) {
         Readable readable = new ShowHandInDeck(blackHands, whiteHands);
         Scanner output = new Scanner(readable);
