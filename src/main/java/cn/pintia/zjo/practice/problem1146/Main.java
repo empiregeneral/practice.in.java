9759cef5ce37e146ada8dcace6279cf57684db28
empiregeneral <13777470884@qq.com>
2020-05-20 12:41:42 +0800

    [ADD] docker-compose.yml

---
+++ src/main/java/cn/pintia/zjo/practice/problem1146/Main.java
@@ -0,0 +1,10 @@
+package cn.pintia.zjo.practice.problem1146;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] arr = new String[][]{{"-", "-"}, {"|", " ", "|"}};
+        System.out.println(Arrays.deepToString(arr));
+    }
+}
--- src/test/java/cn/pintia/zjo/practice/test/problem1093/Problem1093Test.java
+++ src/test/java/cn/pintia/zjo/practice/test/problem1093/Problem1093Test.java
@@ -3,6 +3,8 @@ package cn.pintia.zjo.practice.test.problem1093;
 import cn.pintia.zjo.practice.problem1093.BabylonHeight;
 import cn.pintia.zjo.practice.problem1093.Box;
 import cn.pintia.zjo.practice.problem1093.OrientedBox;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
 import org.junit.Assert;
@@ -14,6 +16,8 @@ import java.util.Collections;
 import java.util.List;
 
 @RunWith(JUnitParamsRunner.class)
+@Epic("动态规划")
+@Feature("求解给定的长、宽、高的盒子所能堆砌的最大高度")
 public class Problem1093Test {
 
     @Test
