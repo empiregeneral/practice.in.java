f29bad9e1987cf2c36e8407a835b53d5d1bf1d70
empiregeneral <13777470884@qq.com>
2020-05-21 12:15:50 +0800

    [MOD] allure-maven 2.10.0

--- src/main/java/cn/pintia/zjo/practice/problem2482/Main.java
+++ src/main/java/cn/pintia/zjo/practice/problem2482/Main.java
@@ -1,7 +1,11 @@
 package cn.pintia.zjo.practice.problem2482;
 
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-
+        String bits = "00000000000000000000000000000000";
+        Pattern pattern =Pattern.compile("(?<=[01])(?=([01]{3}+(?![01])))");
+        pattern.matcher(bits);
     }
 }
