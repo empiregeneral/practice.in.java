65f71316241d5aae477122befbcc4d64db7181a9
empiregeneral <13777470884@qq.com>
2020-09-03 17:54:42 +0800

    [MOD]

---
+++ src/main/java/cn/edu/hdu/acm/problem3117/FibonacciTheory.java
@@ -0,0 +1,65 @@
+package cn.edu.hdu.acm.problem3117;
+
+/**
+ * @author canmozhang
+ */
+public enum FibonacciTheory {
+    GOLD_RATIO(){
+           @Override
+           protected int solution(int n) {
+               double goldenRatio = (1 + Math.sqrt(5)) / 2;
+               return (int)Math.round(Math.pow(goldenRatio, n)/ Math.sqrt(5));
+           }
+        },
+
+        RECURSION() {
+            @Override
+            protected int solution(int n) {
+                if ( n < 0) {
+                    return 1;
+                }
+
+                int[] memo = new int[n+1];
+                java.util.Arrays.fill(memo, 0);
+                return helper(memo, n);
+
+
+            }
+
+            private int helper(int[] memo, int n) {
+                if ( n == 1 || n == 2) {
+                    return n;
+                }
+                if (memo[n] != 0) {
+                    return memo[n];
+                }
+                memo[n] = helper(memo, n - 1) + helper(memo, n-2);
+                return memo[n];
+            }
+
+
+        },
+
+        DYNAMIC_PROGRAMMING() {
+            @Override
+            protected int solution(int n) {
+                if (n < 1) {
+                    return n;
+                }
+                int pre = 0;
+                int cur = 1;
+                int result = 0;
+                for (int i = 0; i < n; i++) {
+                    result = pre + cur;
+                    pre = cur;
+                    cur = result;
+                }
+                return result;
+            }
+        };
+
+
+
+        abstract int solution(int n);
+    }
+
