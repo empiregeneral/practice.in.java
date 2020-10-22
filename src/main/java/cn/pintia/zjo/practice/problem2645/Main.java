d24b281b422c185a6e991c03f3cf6409c08dbc78
empiregeneral <baton789>
2020-10-22 17:45:26 +0800

    [MOD]

--- src/main/java/cn/pintia/zjo/practice/problem2645/Main.java
+++ src/main/java/cn/pintia/zjo/practice/problem2645/Main.java
@@ -1,4 +1,4 @@
-package cn.pintia.zjo.practice.problem2654;
+package cn.pintia.zjo.practice.problem2645;
 
 /**
  * @Description Ip networks 通过位运算求取，ip address的范围
--- src/test/java/cn/pintia/zjo/practice/test/problem2645/Problem2645Test.java
+++ src/test/java/cn/pintia/zjo/practice/test/problem2645/Problem2645Test.java
@@ -1,6 +1,5 @@
 package cn.pintia.zjo.practice.test.problem2645;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import junitparams.JUnitParamsRunner;
