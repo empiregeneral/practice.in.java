a706ca6e08e739f16973b1fa3f3793cee18aceef
empiregeneral <baton789>
2020-11-26 18:06:53 +0800

    [ADD] hdu 1629

---
+++ src/main/java/cn/edu/hdu/acm/problem1629/IHandType.java
@@ -0,0 +1,5 @@
+package cn.edu.hdu.acm.problem1629;
+
+public interface IHandType {
+    HandType getHandType();
+}
--- src/main/java/cn/edu/hdu/acm/problem1629/Main.java
+++ src/main/java/cn/edu/hdu/acm/problem1629/Main.java
@@ -1,7 +1,34 @@
 package cn.edu.hdu.acm.problem1629;
 
+import java.io.BufferedInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    private static final String SEPARATOR = " ";
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(new BufferedInputStream(System.in));
+        while(input.hasNextLine()) {
+            String[] cards = input.nextLine().split(SEPARATOR);
+            List<Card> handCards = new ArrayList<>();
+            List<Card> deckCards = new ArrayList<>();
+            for (int i = 0; i < cards.length; i++) {
+                if (i < 5) {
+                    handCards.add(Card.valueOf(cards[i]));
+                } else {
+                    deckCards.add(Card.valueOf(cards[i]));
+                }
+            }
+
+            Readable readable = new PsychicPokerPlayer(handCards, deckCards);
+            Scanner output = new Scanner(readable);
+            while(output.hasNextLine()) {
+                System.out.println(output.nextLine());
+            }
+        }
     }
+
+
 }
