7b36e9c3a21b29bc3231b9529c0b08b9cfb8ebfe
empiregeneral <baton789>
2020-11-27 14:47:11 +0800

    [ADD] True answer

---
+++ src/main/java/cn/edu/hdu/acm/problem1629/Gambler.java
@@ -0,0 +1,107 @@
+package cn.edu.hdu.acm.problem1629;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Gambler {
+    private HandType bestHandType = HandType.high_card;
+
+    /**
+     * Finds the best hand from the provided sets of cards
+     *
+     * @param handCards
+     *            list of 5 first cards
+     * @param deckCards
+     *            list of 5 last cards
+     * @return
+     */
+    public HandType playCards(List<Card> handCards, List<Card> deckCards) {
+        checkForBestHandType(handCards.toArray(new Card[handCards.size()]));
+
+        Card[] currentHand = new Card[5];
+
+        for (int i = 0; i < 5; i++) {
+            currentHand[i] = new Card(deckCards.get(i).rank, deckCards.get(i).suit);
+            matchWithHandCards(currentHand, handCards, i+1, 0);
+        }
+
+        return bestHandType;
+    }
+
+    /**
+     * Fills in recursively the deck of cards for best hand verification
+     *
+     * @param currentHand
+     *            array of cards for the best hand check
+     * @param handCards
+     *            list of the hand cards
+     * @param currentIdx
+     *            currently processed index
+     * @param startIdx
+     *            index from which to start processing
+     */
+    private void matchWithHandCards(Card[] currentHand, List<Card> handCards, int currentIdx, int startIdx) {
+        if (currentIdx == 5) {
+            checkForBestHandType(currentHand);
+            return;
+        }
+
+        for (int i = startIdx; i < 5; i++) {
+            currentHand[currentIdx] = handCards.get(i);
+            matchWithHandCards(currentHand, handCards, currentIdx + 1, i+1);
+        }
+    }
+
+
+    /**
+     * Verify if particular card set is the best hand
+     *
+     * @param currentCards
+     */
+    private void checkForBestHandType(Card[] currentCards) {
+        List<Card> cardsToCheck = new ArrayList<>();
+        for (Card card : currentCards) {
+            cardsToCheck.add(card);
+        }
+
+        Collections.sort(cardsToCheck);
+
+        Card[] cards = cardsToCheck.toArray(new Card[cardsToCheck.size()]);
+        if (HandTypeEvaluator.hasStraightFlush(cards)) {
+            if (bestHandType.getScores() < HandType.straight_flush.getScores()) {
+                bestHandType = HandType.straight_flush;
+            }
+        } else if (HandTypeEvaluator.hasFourOfAKind(cards)) {
+            if (bestHandType.getScores() < HandType.four_of_a_kind.getScores()) {
+                bestHandType = HandType.four_of_a_kind;
+            }
+        } else if (HandTypeEvaluator.hasFullHouse(cards)) {
+            if (bestHandType.getScores() < HandType.full_house.getScores()) {
+                bestHandType = HandType.full_house;
+            }
+        } else if (HandTypeEvaluator.hasStraight(cards)) {
+            if (bestHandType.getScores() < HandType.straight.getScores()) {
+                bestHandType = HandType.straight;
+            }
+        } else if (HandTypeEvaluator.hasFlush(cards)) {
+            if (bestHandType.getScores() < HandType.flush.getScores()) {
+                bestHandType = HandType.flush;
+            }
+        } else if (HandTypeEvaluator.hasThreeOfAKind(cards)) {
+            if (bestHandType.getScores() < HandType.three_of_a_kind.getScores()) {
+                bestHandType = HandType.three_of_a_kind;
+            }
+        } else if (HandTypeEvaluator.hasTwoPairs(cards)) {
+            if (bestHandType.getScores() < HandType.two_pair.getScores()) {
+                bestHandType = HandType.two_pair;
+            }
+        } else if (HandTypeEvaluator.hasOnePair(cards)) {
+            if (bestHandType.getScores() < HandType.pair.getScores()) {
+                bestHandType = HandType.pair;
+            }
+        }
+
+        return;
+    }
+}
--- src/main/java/cn/edu/hdu/acm/problem1629/HandType.java
+++ src/main/java/cn/edu/hdu/acm/problem1629/HandType.java
@@ -8,7 +8,7 @@ import static java.util.stream.Collectors.toMap;
 
 public enum HandType implements IHandType {
 
-    high_card("high-card", 8) {
+    high_card("highest-card", 0) {
         @Override
         public HandType getHandType() {
             return HandType.high_card;
@@ -20,7 +20,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    pair("pair", 7) {
+    pair("one-pair", 1) {
         @Override
         public HandType getHandType() {
             return HandType.pair;
@@ -32,7 +32,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    two_pair("two-pair", 6) {
+    two_pair("two-pairs", 2) {
         @Override
         public HandType getHandType() {
             return HandType.two_pair;
@@ -44,7 +44,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    three_of_a_kind("three-of-a-kind", 5) {
+    three_of_a_kind("three-of-a-kind", 3) {
         @Override
         public HandType getHandType() {
             return HandType.three_of_a_kind;
@@ -68,7 +68,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    flush("flush", 3) {
+    flush("flush", 5) {
         @Override
         public HandType getHandType() {
             return HandType.flush;
@@ -80,7 +80,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    full_house("full-house", 2) {
+    full_house("full-house", 6) {
         @Override
         public HandType getHandType() {
             return HandType.full_house;
@@ -92,7 +92,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    four_of_a_kind("four-of-a-kind", 1) {
+    four_of_a_kind("four-of-a-kind", 7) {
         @Override
         public HandType getHandType() {
             return HandType.four_of_a_kind;
@@ -104,7 +104,7 @@ public enum HandType implements IHandType {
         }
     },
 
-    straight_flush("straight-flush", 0) {
+    straight_flush("straight-flush", 8) {
         @Override
         public HandType getHandType() {
             return HandType.straight_flush;
