106ed8fb4c856acdbf8020397eef3e015a2a7da4
empiregeneral <13777470884@qq.com>
2022-06-26 23:44:52 +0800

    [MOD] zoj 1002 深度优先搜索，debug信息

--- src/main/java/cn/pintia/zjo/practice/problem1002/Solution.java
+++ src/main/java/cn/pintia/zjo/practice/problem1002/Solution.java
@@ -2,6 +2,7 @@ package cn.pintia.zjo.practice.problem1002;
 
 import java.io.IOException;
 import java.nio.CharBuffer;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,12 +11,14 @@ import java.util.List;
 public class Solution implements Readable{
     private int count = 1;
     private final char[][] board;
-    private int answer;
+    private int answer = 0;
     private final char empty = '.';
+    private final char block = 'X';
+
+    private final char pos = 'O';
 
     public Solution(List<char[]> input) {
         board = input.toArray(new char[input.size()][]);
-        answer = 0;
         solve(0, 0);
     }
 
@@ -25,41 +28,50 @@ public class Solution implements Readable{
 
         if (k == board.length * board.length) {
             answer = (answer > count) ? answer : count;
+            for (int i = 0; i < board[0].length; i++) {
+                System.out.println(Arrays.toString(board[i]));
+            }
+            System.out.println();
         } else {
             x = k / board.length;
             y = k % board.length;
             if ((board[x][y] == empty) && canPos(board, x, y)) {
-                board[x][y] = 'O';
+                board[x][y] = pos;
                 solve(k + 1, count+1);
-                board[x][y] = '.';
+                board[x][y] = empty;
             }
+            for (int i = 0; i < board[0].length; i++) {
+                System.out.println(Arrays.toString(board[i]));
+            }
+            System.out.println();
             solve(k+1, count);
         }
     }
 
     private boolean canPos(char[][] board, int r, int c) {
 
-        if (board[r][c] == 'X') {
+        if (board[r][c] == block) {
             return false;
         }
 
         for ( int col = c - 1; col >= 0; col--) {
-            if (board[r][col] == 'X') {
+            if (board[r][col] == block) {
                 break;
             }
-            if (board[r][col] == 'O') {
+            if (board[r][col] == pos) {
                 return false;
             }
         }
 
         for ( int row = r -1; row >= 0;  row--) {
-            if (board[row][c] == 'X') {
+            if (board[row][c] == block) {
                 break;
             }
-            if (board[row][c] == 'O') {
+            if (board[row][c] == pos) {
                 return false;
             }
         }
+
         return true;
     }
 
